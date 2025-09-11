3680ac61fdb076f808d85d405425dbef43538a8a
IvonWei <madwind.cn@gmail.com>
2025-09-11 16:17:58 +0800

    Create k3s-client-init.sh

---
+++ k3s-client-init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# -----bbr-----
+echo "Enabling BBR..."
+sudo sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sudo sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+lsmod | grep bbr
+sudo sysctl -a | grep tcp_congestion_control
+
+# -----ip_vs-----
+echo "Enabling IPVS..."
+sudo modprobe ip_vs
+echo "ip_vs" | sudo tee -a /etc/modules-load.d/ipvs.conf
+lsmod | grep ip_vs
+
+# -----iptables-----
+echo "Setting up iptables rules..."
+sudo tee /etc/rc.local > /dev/null <<'EOF'
+#!/bin/bash
+sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -I INPUT -p udp --dport 51820 -j ACCEPT
+sudo iptables -I INPUT -p udp --dport 51821 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 10250 -j ACCEPT
+sudo iptables -I INPUT -s 10.42.0.0/16 -j ACCEPT
+sudo iptables -I INPUT -s 10.43.0.0/16 -j ACCEPT
+EOF
+sudo chmod +x /etc/rc.local
+sudo /etc/rc.local
+
+# -----uninstall previous k3s-----
+echo "Uninstalling previous K3s installation..."
+if command -v k3s >/dev/null 2>&1; then
+    k3s-uninstall.sh
+elif command -v k3s-agent >/dev/null 2>&1; then
+    k3s-agent-uninstall.sh
+fi
+
+# -----k3s installation-----
+echo "Installing K3s..."
+HOSTNAME=$(hostname)
+CLUSTER_NAME=${HOSTNAME::-1}
+SERVER_IP=$(curl -s -X POST https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/d1/database/$DATABASE_ID/query \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -d '{
+          "sql": "SELECT ip FROM config WHERE cluster_name = ?;",
+          "params": ["'$CLUSTER_NAME'"]
+        }' | jq -r '.result[0].results[0].ip')
+export K3S_URL=https://${SERVER_IP}:6443
+export INSTALL_K3S_EXEC="
+--kube-proxy-arg proxy-mode=ipvs
+"
+curl -sfL https://get.k3s.io | sh -
+echo "done."
