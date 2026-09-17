8527c8dd474b5c99c1c14888d56bb0fda7b0361f
IvonWei <madwind.cn@gmail.com>
2026-09-17 23:38:47 +0800

    Add client preflight validation

--- k3s-client-init.sh
+++ k3s-client-init.sh
@@ -20,6 +20,11 @@ fi
 # shellcheck source=lib/common.sh
 source "$COMMON_SH"
 
+# -----preflight-----
+preflight_require_root
+preflight_require_env DOMAIN K3S_TOKEN
+preflight_require_commands curl hostname sed awk grep install systemctl sysctl modprobe getent
+
 # -----host setup-----
 enable_bbr
 enable_ipvs
